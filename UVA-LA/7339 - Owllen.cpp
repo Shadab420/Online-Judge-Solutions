0d6cd99347b1eb89645f23e08d99658b6e958caf
Md. Kazi Shadab Anwar <shadab_anwar@outlook.com>
2016-09-27 20:07:33 +0600

    Add files via upload

---
+++ UVA-LA/7339 - Owllen.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/******************** START OF CRAPS ********************/
+
+#define fin             freopen("input/in.txt","r",stdin);
+#define fout            freopen("input/out.txt","w",stdout);
+#define ri(x)           scanf("%d", &x)
+#define r2i(x,y)        scanf("%d %d", &x, &y)
+#define rll(x)          scanf("%lld", &x)
+#define r2ll(x,y)       scanf("%lld %lld", &x, &y)
+#define imax            INT_MAX
+#define imin            INT_MIN
+#define pb              push_back
+#define mp              make_pair
+#define pi              acos(-1.0)
+#define newl            "\n"
+#define newlChar        '\n'
+#define spChar          ' '
+#define max3(a,b,c)     max(a,max(b,c))
+#define min3(a,b,c)     min(a,min(b,c))
+#define pCase(cs,ch)    printf("Case %d:%c", cs,ch)
+#define iterAll(x)      x.begin(),x.end()
+#define rIterAll(x)     x.rbegin(),x.rend()
+#define fastreader      ios_base::sync_with_stdio(false); cin.tie(0)
+
+typedef long long               ll;
+typedef unsigned long long      ull;
+typedef vector<int>             vi;
+typedef vector<ll>              vll;
+typedef vector<double>          vd;
+typedef vector<string>          vs;
+typedef vector<char>            vc;
+typedef pair<int,int>           pii;
+typedef map<int,int>            mapii;
+typedef map<char,int>           mapci;
+typedef map<string,int>         mapsi;
+
+/******************** END OF CRAPS ********************/
+
+int main()
+{
+    //fin; fout;
+
+    int t; ri(t);
+    string s;
+
+    for(int cs=1; cs<=t; cs++)
+    {
+        cin>>s;
+
+        int alpha[26] = {0};
+
+        for(int i=0; i<s.length(); i++) alpha[s[i]-'a']++;
+
+        sort(alpha, alpha+26);
+
+        cout << "Case " << cs << ": " << alpha[0] << endl;
+    }
+
+
+    return 0;
+}
