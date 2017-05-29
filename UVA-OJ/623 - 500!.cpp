c51a27f7ae652fdecd432d21fb2d6e2f5a7cd66a
Md. Kazi Shadab Anwar <shadab_anwar@outlook.com>
2017-05-29 13:08:45 +0600

    Add files via upload

---
+++ UVA-OJ/623 - 500!.cpp
@@ -0,0 +1,93 @@
+///Verdict: AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/******************** START OF CRAPS ********************/
+
+#define fin             freopen("input/in.txt","r",stdin);
+#define fout            freopen("input/out.txt","w",stdout);
+#define ri(x)           scanf("%d", &x)
+#define r2i(x,y)        scanf("%d %d", &x, &y)
+#define rll(x)          scanf("%lld", &x)
+#define r2ll(x,y)       scanf("%lld %lld", &x, &y)
+#define imax            INT_MAX
+#define imin            INT_MIN
+#define pb              push_back
+#define mp              make_pair
+#define pi              acos(-1.0)
+#define newl            "\n"
+#define newlChar        '\n'
+#define spChar          ' '
+#define max3(a,b,c)     max(a,max(b,c))
+#define min3(a,b,c)     min(a,min(b,c))
+#define pCase(cs,ch)    printf("Case %d:%c", cs,ch)
+#define iterAll(x)      x.begin(),x.end()
+#define rIterAll(x)     x.rbegin(),x.rend()
+#define fastreader      ios_base::sync_with_stdio(false); cin.tie(0)
+
+typedef long long               ll;
+typedef unsigned long long      ull;
+typedef vector<int>             vi;
+typedef vector<ll>              vll;
+typedef vector<double>          vd;
+typedef vector<string>          vs;
+typedef vector<char>            vc;
+typedef pair<int,int>           pii;
+typedef map<int,int>            mapii;
+typedef map<char,int>           mapci;
+typedef map<string,int>         mapsi;
+
+/******************** END OF CRAPS ********************/
+
+int res[10001];
+
+int multiply(int d, int sz)
+{
+    int carry = 0;
+
+    for(int i=0; i<sz; i++)
+    {
+        int product = res[i]*d + carry;
+        res[i] = product%10;
+        carry = product/10;
+    }
+
+    while(carry)
+    {
+        res[sz] = carry%10;
+        carry/=10;
+        sz++;
+    }
+
+    return sz;
+}
+
+void factorial(int n)
+{
+    res[0] = 1;
+    int sz=1;
+
+    for(int i=2; i<=n; i++)
+    {
+        sz = multiply(i,sz);
+    }
+
+    for(int i=sz-1; i>=0; i--) printf("%d", res[i]);
+    puts("");
+}
+
+int main()
+{
+    fin; fout;
+
+    int n;
+
+    while(ri(n)!=EOF)
+    {
+        printf("%d!\n",n);
+        factorial(n);
+    }
+
+    return 0;
+}
